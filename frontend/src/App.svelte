6d2a08e1e90f63357ffe9be77eb9f8377ad1ffa3
swz-git <contact@swz.works>
2022-07-30 02:59:22 +0200

    frontend stuff

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,58 @@
 <script>
+  import { Greet } from "../wailsjs/go/main/App";
+  import spinner from "./assets/spinner.svg";
+  let loading = false;
+  async function search() {
+    loading = true;
+    // actually search
+    loading = false;
+  }
 </script>
 
 <main>
-  <h1>PeerTV</h1>
+  <header>
+    <h1>PeerTV</h1>
+    <div class="search">
+      <input type="text" />
+      <button on:click={search}>Search</button>
+    </div>
+    {#if loading}
+      <img src={spinner} alt="" srcset="" />
+    {/if}
+  </header>
 </main>
 
 <style>
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    background-color: transparent;
+  }
+  header h1 {
+    margin: 0; /*stfu*/
+    position: fixed;
+    left: 1rem;
+  }
+
+  .search {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1px;
+    border-radius: 1rem;
+  }
+  .search * {
+    padding: 0.4rem;
+    margin: 0;
+    border: 0;
+    font-size: 1.3rem;
+    background-color: #323232;
+    color: white;
+    outline: none;
+  }
+  .search button {
+    cursor: pointer;
+  }
 </style>
